95a6a3c9aaa224917a660e01a99c27cdce1f76ba
binmw <bintangmwiratama@gmail.com>
2026-01-21 14:59:26 +0700

    update : rincian status

--- resources/views/dashboard/mahasiswa/index.blade.php
+++ resources/views/dashboard/mahasiswa/index.blade.php
@@ -1,133 +1,131 @@
 @extends('layouts.main')
 @section('content')
-
-<style>
-    .page-wrapper {
-        padding: 40px;
-    }
-
-    .custom-card {
-        background: #fff;
-        border-radius: 12px;
-        box-shadow: 0 4px 12px rgba(0,0,0,0.05);
-        height: 100%;
-        padding: 18px 20px;
-        border: none;
-    }
-
-    .btn-ungu {
-        background-color: #9A5EB1;
-        color: #fff;
-        font-weight: 600;
-    }
-
-    .btn-ungu:hover {
-        background-color: #995eb1d8;
-        color: #fff;
-    }
-
-    @media (max-width: 768px) {
+    <style>
         .page-wrapper {
-            padding: 20px;
+            padding: 40px;
         }
-    }
 
-    .img-dashboard{
-        margin-top: 40px;
-        text-align: center;
-    }
+        .custom-card {
+            background: #fff;
+            border-radius: 12px;
+            box-shadow: 0 4px 12px rgba(0, 0, 0, 0.05);
+            height: 100%;
+            padding: 18px 20px;
+            border: none;
+        }
 
-    .img-dashboard img{
-        max-width: 500px;
-    }
+        .btn-ungu {
+            background-color: #9A5EB1;
+            color: #fff;
+            font-weight: 600;
+        }
 
-    .welcome-text{
-        margin-bottom: 20px;
-    }
+        .btn-ungu:hover {
+            background-color: #995eb1d8;
+            color: #fff;
+        }
 
-</style>
+        @media (max-width: 768px) {
+            .page-wrapper {
+                padding: 20px;
+            }
+        }
 
-<div class="page-wrapper">
-    <div class="row g-4 align-items-stretch">
+        .img-dashboard {
+            margin-top: 40px;
+            text-align: center;
+        }
 
-        <div class="col-md-6 d-flex">
-            <div class="w-100 px-md-4">
+        .img-dashboard img {
+            max-width: 500px;
+        }
 
-                 <div class="welcome-text">
-                    Halo, <strong>{{ auth()->user()->name }}</strong> 👋  
+        .welcome-text {
+            margin-bottom: 20px;
+        }
+    </style>
+
+    <div class="page-wrapper">
+        <div class="row g-4 align-items-stretch">
+
+            <div class="col-md-6 d-flex">
+                <div class="w-100 px-md-4">
+
+                    <div class="welcome-text">
+                        Halo, <strong>{{ auth()->user()->name }}</strong> 👋
+                    </div>
+
+                    <h3 class="fw-bold mb-3">
+                        Selamat Datang di Website Pengaduan!
+                    </h3>
+
+                    <p class="text-muted mb-4">
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+                        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                        quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                        consequat.
+                    </p>
+
+                    <div class="img-dashboard">
+                        <img src="{{ asset('assets/img/vector-2.png') }}" alt="Ilustrasi Pengaduan">
+                    </div>
+
                 </div>
-
-                <h3 class="fw-bold mb-3">
-                    Selamat Datang di Website Pengaduan!
-                </h3>
-
-                <p class="text-muted mb-4">
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-                    tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-                    quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-                    consequat.
-                </p>
-
-                <div class="img-dashboard">
-                    <img 
-                        src="{{ asset('assets/img/vector-2.png') }}" 
-                        alt="Ilustrasi Pengaduan"
-                    >
-                </div>
-
             </div>
-        </div>
 
-        <div class="col-md-6">
-            <div class="card custom-card h-100">
-                <div class="card-body">
-                    <h5 class="mb-3">Form Laporan Pengaduan</h5>
+            <div class="col-md-6">
+                <div class="card custom-card h-100">
+                    <div class="card-body">
+                        <h5 class="mb-3">Form Laporan Pengaduan</h5>
 
-                    <form action="{{ route('mahasiswa.pengaduan.store') }}" method="POST" enctype="multipart/form-data">
-                        @csrf
+                        <form action="{{ route('mahasiswa.pengaduan.store') }}" method="POST"
+                            enctype="multipart/form-data">
+                            @csrf
 
-                        <div class="mb-3">
-                            <label class="form-label">Nama</label>
-                            <input type="text" class="form-control" placeholder="{{ auth()->user()->name }}" disabled>
-                        </div>
-                        <div class="mb-3">
-                            <label class="form-label">NPM</label>
-                            <input type="text" class="form-control" placeholder="{{ auth()->user()->npm }}" disabled>
-                        </div>
-                        
-                        <div class="mb-3">
-                            <label class="form-label">Kategori</label>
-                            <select class="form-select" name="kategori">
-                                <option value="">-- pilih kategori --</option>
-                                @foreach ($kategori as $row)
-                                    <option value="{{ $row }}">{{ $row }}</option>
-                                @endforeach
-                            </select>
-                        </div>
+                            <div class="mb-3">
+                                <label class="form-label">Nama</label>
+                                <input type="text" class="form-control" placeholder="{{ auth()->user()->name }}"
+                                    disabled>
+                            </div>
+                            <div class="mb-3">
+                                <label class="form-label">NPM</label>
+                                <input type="text" class="form-control" placeholder="{{ auth()->user()->npm }}" disabled>
+                            </div>
 
-                        <div class="mb-3">
-                            <label class="form-label">Keluhan</label>
-                            <textarea name="keluhan" class="form-control" rows="4" placeholder="Tuliskan keluhan anda"></textarea>
-                        </div>
+                            <div class="mb-3">
+                                <label class="form-label">Kategori</label>
+                                <select class="form-select" name="kategori">
+                                    <option value="">-- pilih kategori --</option>
+                                    @foreach ($kategori as $row)
+                                        <option value="{{ $row }}">{{ $row }}</option>
+                                    @endforeach
+                                </select>
+                            </div>
 
-                        <div class="mb-3">
-                            <label class="form-label">Tingkat / Level</label>
-                            <select class="form-select" name="tingkat_kepentingan">
-                                <option value="">-- pilih level --</option>
-                                @foreach ($tingkat_kepentingan as $row)
-                                    <option value="{{ $row }}">{{ $row }}</option>
-                                @endforeach
-                            </select>
-                        </div>
+                            <div class="mb-3">
+                                <label class="form-label">Keluhan</label>
+                                <textarea name="keluhan" class="form-control" rows="4" placeholder="Tuliskan keluhan anda"></textarea>
+                            </div>
 
-                        <div class="mb-3">
-                            <label class="form-label">Bukti</label>
-                            <input type="file" class="form-control" name="bukti" id="fileUpload" accept="image/*">
-                            <img id="imagePreview" src=""alt="Preview Bukti"style="display:none; margin-top:10px; max-width:200px;">
-                        </div>
+                            <div class="mb-3">
+                                <label class="form-label">Tingkat / Level</label>
+                                <select class="form-select" name="tingkat_kepentingan">
+                                    <option value="">-- pilih level --</option>
+                                    @foreach ($tingkat_kepentingan as $row)
+                                        <option value="{{ $row }}">{{ $row }}</option>
+                                    @endforeach
+                                </select>
+                            </div>
+
+                            <div class="mb-3">
+                                <label class="form-label">Bukti</label>
+                                <input type="file" class="form-control" name="bukti" id="fileUpload" accept="image/*">
+                                <img id="imagePreview" src=""alt="Preview Bukti"
+                                    style="display:none; margin-top:10px; max-width:200px;">
+                            </div>
 
 
-                         <script>
+                            <script>
                                 document.getElementById('fileUpload').addEventListener('change', function() {
                                     const file = this.files[0];
                                     if (file) {
@@ -141,23 +139,20 @@
                                 });
                             </script>
 
-                        <button type="submit" class="btn btn-ungu w-100 mt-2">
-                            Kirim Laporan
-                        </button>
-                        @if ($errors->any())
-                            <div class="alert alert-danger">
-                                {{ $errors->first() }}
-                            </div>
-                        @endif
+                            <button type="submit" class="btn btn-ungu w-100 mt-2">
+                                Kirim Laporan
+                            </button>
+                            @if ($errors->any())
+                                <div class="alert alert-danger">
+                                    {{ $errors->first() }}
+                                </div>
+                            @endif
 
-                    </form>
+                        </form>
+                    </div>
                 </div>
             </div>
+
         </div>
-
     </div>
-</div>
-
-       
-
 @endsection
--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -10,9 +11,10 @@
 </head>
 
 <style>
-    body{
+    body {
         background-color: #F6F5FB
     }
+
     .profile-icon {
         width: 38px;
         height: 38px;
@@ -25,23 +27,23 @@
         background-color: #f3f0ff;
     }
 
-    .nav-item .nav-link{
-        color: #1F2937 !important; 
+    .nav-item .nav-link {
+        color: #1F2937 !important;
         font-weight: 400;
     }
 
     .nav-item .nav-link.active {
-        color: #9A5EB1 !important; 
+        color: #9A5EB1 !important;
         font-weight: 700;
     }
 
     .navbar {
-    transition: box-shadow 0.3s ease;
+        transition: box-shadow 0.3s ease;
     }
 
-    
+
     .footer {
-        background-color: #9A5EB1; 
+        background-color: #9A5EB1;
         color: #ffffff;
         text-align: center;
         padding: 12px 20px;
@@ -52,96 +54,97 @@
 
 <body>
 
-<nav class="navbar navbar-expand-lg navbar-light bg-white shadow-sm py-3 sticky-top">
-    <div class="container">
-        <a class="navbar-brand d-flex align-items-center gap-2" href="{{ route('mahasiswa.pengaduan') }}">
-            <img src="{{ asset('assets/img/logo-wp-new.png') }}" alt="Logo" height="50">
-        </a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarMenu">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarMenu">
-            <ul class="navbar-nav mx-auto mb-2 mb-lg-0 gap-3">
-                <li class="nav-item">
-                    <a class="nav-link {{ request()->routeIs('mahasiswa.pengaduan') ? 'active' : '' }}" 
-                    href="{{ route('mahasiswa.pengaduan') }}">
-                        Beranda
-                    </a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link {{ request()->routeIs('mahasiswa.semua.laporan') ? 'active' : '' }}" href="{{ route('mahasiswa.semua.laporan') }}">
-                        Cek Laporan
-                    </a>
-                </li>
-            </ul>
-            <div class="dropdown">
-                <a class="d-flex align-items-center justify-content-center rounded-circle border profile-icon"
-                   href="#" data-bs-toggle="dropdown">
-                    <i class="bi bi-person"></i>
-                </a>
-
-                <ul class="dropdown-menu dropdown-menu-end">
-                    <li>
-                        <a class="dropdown-item" href="#">
-                            Profil
+    <nav class="navbar navbar-expand-lg navbar-light bg-white shadow-sm py-3 sticky-top">
+        <div class="container">
+            <a class="navbar-brand d-flex align-items-center gap-2" href="{{ route('mahasiswa.pengaduan') }}">
+                <img src="{{ asset('assets/img/logo-wp-new.png') }}" alt="Logo" height="50">
+            </a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarMenu">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarMenu">
+                <ul class="navbar-nav mx-auto mb-2 mb-lg-0 gap-3">
+                    <li class="nav-item">
+                        <a class="nav-link {{ request()->routeIs('mahasiswa.pengaduan') ? 'active' : '' }}"
+                            href="{{ route('mahasiswa.pengaduan') }}">
+                            Beranda
                         </a>
                     </li>
-                    <li>
-                        <hr class="dropdown-divider">
-                    </li>
-                    <li>
-                        <form id="form-logout" action="{{ route('logout') }}" method="POST">
-                            @csrf
-                            <button type="button" class="dropdown-item text-danger"
-                                onclick="konfirmasiLogout()">
-                                Logout
-                            </button>
-                        </form>
+                    <li class="nav-item">
+                        <a class="nav-link {{ request()->routeIs('mahasiswa.semua.laporan') ? 'active' : '' }}"
+                            href="{{ route('mahasiswa.semua.laporan') }}">
+                            Cek Laporan
+                        </a>
                     </li>
                 </ul>
+                <div class="dropdown">
+                    <a class="d-flex align-items-center justify-content-center rounded-circle border profile-icon"
+                        href="#" data-bs-toggle="dropdown">
+                        <i class="bi bi-person"></i>
+                    </a>
+
+                    <ul class="dropdown-menu dropdown-menu-end">
+                        <li>
+                            <a class="dropdown-item" href="#">
+                                Profil
+                            </a>
+                        </li>
+                        <li>
+                            <hr class="dropdown-divider">
+                        </li>
+                        <li>
+                            <form id="form-logout" action="{{ route('logout') }}" method="POST">
+                                @csrf
+                                <button type="button" class="dropdown-item text-danger" onclick="konfirmasiLogout()">
+                                    Logout
+                                </button>
+                            </form>
+                        </li>
+                    </ul>
+                </div>
             </div>
+
         </div>
+    </nav>
+
+    <div class="content-wrapper">
+
+        @yield('content')
+
 
     </div>
-</nav>
-
-<div class="content-wrapper">
-
-            @yield('content')
 
 
-</div>
+    <footer class="footer">
+        <p>
+            ©2025 Kelompok 2. All right reserved.<br>
+            Jl. Rangga Gading No.1, Gudang, Kecamatan Bogor Tengah, Kota Bogor, Jawa Barat, 16123.
+            WA: 0813-1377-7672
+        </p>
+    </footer>
 
-
- <footer class="footer">
-            <p>
-                ©2025 Kelompok 2. All right reserved.<br>
-                Jl. Rangga Gading No.1, Gudang, Kecamatan Bogor Tengah, Kota Bogor, Jawa Barat, 16123. 
-                WA: 0813-1377-7672
-            </p>
-        </footer>
-
-<script>
-function konfirmasiLogout() {
-    Swal.fire({
-        title: 'Yakin mau logout?',
-        text: 'Kamu akan keluar dari sesi login',
-        icon: 'question',
-        showCancelButton: true,
-        confirmButtonText: 'Ya, logout',
-        cancelButtonText: 'Batal'
-    }).then((result) => {
-        if (result.isConfirmed) {
-            document.getElementById('form-logout').submit();
+    <script>
+        function konfirmasiLogout() {
+            Swal.fire({
+                title: 'Yakin mau logout?',
+                text: 'Kamu akan keluar dari sesi login',
+                icon: 'question',
+                showCancelButton: true,
+                confirmButtonText: 'Ya, logout',
+                cancelButtonText: 'Batal'
+            }).then((result) => {
+                if (result.isConfirmed) {
+                    document.getElementById('form-logout').submit();
+                }
+            });
         }
-    });
-}
-</script>
+    </script>
 
 
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
-<script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
 
 
 </body>
+
 </html>
